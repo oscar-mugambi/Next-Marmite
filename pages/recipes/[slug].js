0a1ee9644eefbb2c2f7f3c4c6d52ef207f2bfd5e
Oscar Mugambi <oscarmugambi@gmail.com>
2021-06-28 09:39:38 +0300

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful"
 import Image from "next/image"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Skeleton from "../../components/Skeleton"
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -16,7 +17,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -35,6 +36,10 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) {
+    return <Skeleton />
+  }
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields
 
